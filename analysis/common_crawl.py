5240a311cd44ddfae46e529f86d353a878005e67
reinvantveer <reinvantveer@gmail.com>
2022-10-07 13:01:03 +0200

    bootstrap analysis function, skip analysis before starting point

--- analysis/common_crawl.py
+++ analysis/common_crawl.py
@@ -1,5 +1,7 @@
 import json
+import logging
 from argparse import ArgumentParser
+from collections import Counter
 
 from analysis.config import load_config, Config
 
@@ -7,6 +9,17 @@ from analysis.config import load_config, Config
 def main(config: Config):
     with open(config['data']['common_crawl']['index_file'], 'rt') as f:
         cc_roots = json.loads(f.read())
+
+        for root in cc_roots:
+            if root['id'] < config['data']['common_crawl']['start_from']:
+                continue
+
+            logging.info(f'Analysing {root["name"]}')
+            analysis = analyse(root['name'], )
+
+
+def analyse(root_url: str) -> Counter:
+    return Counter()
 
 
 if __name__ == '__main__':
