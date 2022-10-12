92d0045f5676e3c6cec3575217897a22ee35b695
reinvantveer <reinvantveer@gmail.com>
2022-10-12 19:38:02 +0200

    WIP: add beginning of declining mime types filter
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/common_crawl.py
+++ analysis/common_crawl.py
@@ -11,6 +11,11 @@ StatsDict = TypedDict('StatsDict',
                       {'crawl': str, 'mimetype_detected': str, 'pages': int, 'urls': int, 'pct_pages_per_crawl': float})
 StatsDictTable = List[StatsDict]
 
+Crawl, PageCount = str, int
+MimeStats = Dict[Crawl, PageCount]
+MimeType = str
+MimeDict = Dict[MimeType, MimeStats]
+
 
 def main(config: Config) -> None:
     crawl_cfg = config['data']['common_crawl']
@@ -47,11 +52,27 @@ def parse_csv(stats: List[Dict[str, str]]) -> StatsDictTable:
     return stats_dict
 
 
-def filter_declining(typed_stats: StatsDictTable) -> StatsDictTable:
-    return typed_stats
+def filter_declining(typed_stats: StatsDictTable) -> MimeDict:
+    """
+    Filters the list of statistics for MIME types that decline over the last year
+
+    :param typed_stats: a list of dictionaries with typed values
+
+    :return: a dictionary of mime types with declining counts, with the count per year
+    """
+    declining_mime_types = {}
+
+    # First: "de-normalize" the table into a nested dictionary of mime types with page counts per crawl
+    # This is easier to handle: we want to analyse statistics per mime type, over the years
+    mime_types = set([stat['mimetype_detected'] for stat in typed_stats])
+    for row in typed_stats:
+        declining_mime_types.setdefault(row['mimetype_detected'], 0)
+        declining_mime_types[row['mimetype_detected']] += row['pages']
+
+    return declining_mime_types
 
 
-def analyse(stats: StatsDictTable) -> None:
+def analyse(stats: MimeDict) -> None:
     pass
 
 
