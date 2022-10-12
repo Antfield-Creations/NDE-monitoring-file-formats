0f9a22081d7d168d9cc00652bba73610e7bb27d0
reinvantveer <reinvantveer@gmail.com>
2022-10-12 13:04:51 +0200

    drop redundant shell script to re-generate raw data sample
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- tests/testdata/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Get a random cdx gzip file from Common Crawl
-wget 'https://data.commoncrawl.org/cc-index/collections/CC-MAIN-2017-39/indexes/cdx-00000.gz'
-# Unzip it
-gunzip cdx-00000.gz
-# Extract the first 100 entries and re-gzip to a smaller sample gzip file
-head -n 100 cdx-00000 | gzip -c > ~/Documents/Git/NDE-monitoring-file-formats/tests/testdata/sample.gz
-# Ditch the large gzip file
-rm cdx-00000.gz
