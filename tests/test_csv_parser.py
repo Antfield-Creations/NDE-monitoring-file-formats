0bb58a4027bb2122c4c804ddc4b02fc652ab4ad6
reinvantveer <reinvantveer@gmail.com>
2022-10-12 15:05:09 +0200

    add csv parser test
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

---
+++ tests/test_csv_parser.py
@@ -0,0 +1,12 @@
+from analysis.common_crawl import parse_csv
+
+sample = [
+    {'crawl': 'CC-MAIN-2017-22', 'mimetype_detected': '<other>', 'pages': '410273', 'urls': '408769',
+     '%pages/crawl': '0.0138'},
+]
+
+
+def test_parse_csv() -> None:
+    parsed = parse_csv(sample)
+    assert type(parsed[0]['urls']) == int
+    assert type(parsed[0]['pct_pages_per_crawl']) == float
