a0a3d8bda49fdfb4388e6e4ccd8a1b71f2845983
reinvantveer <reinvantveer@gmail.com>
2022-10-12 10:38:43 +0200

    download cdx file and count mime types for urls that crawled OK
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/common_crawl.py
+++ analysis/common_crawl.py
@@ -2,8 +2,10 @@ import datetime
 import gzip
 import json
 import logging
+import os
 from argparse import ArgumentParser
 from collections import Counter
+from urllib import request
 
 from analysis.config import load_config, Config
 
@@ -13,16 +15,49 @@ def main(config: Config):
     with open(crawl_cfg['index_file'], 'rt') as f:
         cc_roots = json.loads(f.read())
 
-        for root in cc_roots:
-            if root['id'] < config['data']['common_crawl']['start_from']:
+    for crawl_month in cc_roots:
+        if crawl_month['id'] < crawl_cfg['start_from']:
+            continue
+
+        url = crawl_cfg['url_index_download_template'].format(crawl_entry=crawl_month['id'])
+        logging.info(f'Retrieving {url}')
+        gzip_path = os.path.join(crawl_cfg['download_dir'], 'cc-index.paths.gz')
+        request.urlretrieve(url, gzip_path)
+
+        with gzip.open(gzip_path, 'rb') as f:
+            cdx_urls = f.read().decode()
+
+        for index in cdx_urls.split('\n'):
+            filename = index.split('/')[-1]
+            cdx_gz = os.path.join(crawl_cfg['download_dir'], filename)
+            logging.info(f'Retrieving {index} to {cdx_gz}')
+            request.urlretrieve(index, cdx_gz)
+
+            logging.info(f'Analysing {index}')
+            analysis = analyse(cdx_gz)
+            print(analysis)
+
+
+def analyse(gzip_path) -> Counter:
+    counter = Counter()
+
+    with gzip.open(gzip_path, 'rt') as f:
+        while True:
+            url = f.readline()
+            if url is None or url == '':
+                break
+
+            json_start = url.find('{"url"')
+            entry_json = url[json_start:]
+            entry = json.loads(entry_json)
+
+            # Handle only response OK entries
+            if entry['status'] != '200':
                 continue
 
-            logging.info(f'Analysing {root["name"]}')
-            analysis = analyse(root['name'], )
+            counter.update([entry['mime-detected']])
 
-
-def analyse(root_url: str) -> Counter:
-    return Counter()
+    return counter
 
 
 if __name__ == '__main__':
