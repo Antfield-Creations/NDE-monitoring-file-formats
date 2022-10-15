d0fd7a9f85019c684844dea091cac066fde5119b
reinvantveer <rein@vantveer.me>
2022-10-15 15:59:12 +0200

    improve code style through spacing and wrapping in main function
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- models/bass_diffusion.py
+++ models/bass_diffusion.py
@@ -33,38 +33,34 @@ def main() -> None:
     sales = np.array([840, 1470, 2110, 4000, 7590, 10950, 10530, 9470, 7790, 5890])
     cumulative_sales = np.cumsum(sales)
 
-# residual (error) function
-def residual(vars, t, sales):
-    M = vars[0]
-    P = vars[1]
-    Q = vars[2]
-    Bass = M * (((P+Q)**2/P)*np.exp(-(P+Q)*t))/(1+(Q/P)*np.exp(-(P+Q)*t))**2
-    return (Bass - (sales))
+    # initial variables(m, p & q)
+    m, p, q = 60630, 0.03, 0.38
 
-# non linear least square fitting
-varfinal,success = leastsq(residual, vars, args=(t, sales))
+    # non-linear least square fitting
+    varfinal = leastsq(residual, np.array([m, p, q]), args=(t, sales))
 
-# estimated coefficients
-m = varfinal[0]
-p = varfinal[1]
-q = varfinal[2]
+    # estimated coefficients
+    m = varfinal[0]
+    p = varfinal[1]
+    q = varfinal[2]
+
+    # sales plot (pdf)
+    # time interpolation
+    tp = np.linspace(1.0, 100.0, num=100) / 10
+    cofactor = np.exp(-(p + q) * tp)
+    sales_pdf = m * (((p + q) ** 2 / p) * cofactor) / (1 + (q / p) * cofactor) ** 2
+    plt.plot(tp, sales_pdf, t, sales)
+    plt.title('Sales pdf')
+    plt.legend(['Fit', 'True'])
+    plt.show()
+
+    # Cumulative sales (cdf)
+    sales_cdf = m * (1 - cofactor) / (1 + (q / p) * cofactor)
+    plt.plot(tp, sales_cdf, t, cumulative_sales)
+    plt.title('Sales cdf')
+    plt.legend(['Fit', 'True'])
+    plt.show()
 
 
-print(varfinal)
-#sales plot (pdf)
-#time interpolation
-tp=(np.linspace(1.0, 100.0, num=100))/10
-cofactor= np.exp(-(p+q) * tp)
-sales_pdf= m* (((p+q)**2/p)*cofactor)/(1+(q/p)*cofactor)**2
-plt.plot(tp, sales_pdf,t,sales)
-plt.title('Sales pdf')
-plt.legend(['Fit', 'True'])
-plt.show()
-
-
-# Cumulative sales (cdf)
-sales_cdf= m*(1-cofactor)/(1+(q/p)*cofactor)
-plt.plot(tp, sales_cdf,t,c_sales)
-plt.title('Sales cdf')
-plt.legend(['Fit', 'True'])
-plt.show()
+if __name__ == '__main__':
+    main()
