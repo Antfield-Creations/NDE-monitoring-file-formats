748c3738bc3f65322e0f68e8b6080163c8450d46
reinvantveer <rein@vantveer.me>
2022-10-15 15:58:27 +0200

    refactor: proper docstring, imports reordering, introduce main function

--- models/bass_diffusion.py
+++ models/bass_diffusion.py
@@ -3,18 +3,35 @@ This module was adapted from
 https://github.com/NForouzandehmehr/Bass-Diffusion-model-for-short-life-cycle-products-sales-prediction/blob/master/bass.py
 """
 
-from scipy.optimize import leastsq
-from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 import numpy as np
-#time intervals
-t= np.linspace(1.0, 10.0, num=10)
-# sales vector
-sales=np.array([840,1470,2110,4000, 7590, 10950, 10530, 9470, 7790, 5890])
-# cumulatice sales
-c_sales=np.cumsum(sales)
-# initial variables(M, P & Q)
-vars = [60630, 0.03,0.38]
+from scipy.optimize import leastsq
+
+
+def residual(m, p, q, t, sales):
+    """
+    Residual error function that calculates the difference between the Bass model output given coefficients
+    m, p, q at time t (see below) on the one hand and the actual sales figure on the other hand.
+
+    :param m:       Market potential coefficient
+    :param p:       Innovators coefficient
+    :param q:       Imitators coefficient
+    :param t:       Time
+    :param sales:   The actual sales value
+
+    :return: The difference between the calculated Bass function output and the actual provided sales figure
+    """
+    bass = m * (((p + q) ** 2 / p) * np.exp(-(p + q) * t)) / (1 + (q / p) * np.exp(-(p + q) * t)) ** 2
+    return bass - sales
+
+
+def main() -> None:
+    # time intervals
+    t = np.linspace(1.0, 10.0, num=10)
+
+    # sales vector
+    sales = np.array([840, 1470, 2110, 4000, 7590, 10950, 10530, 9470, 7790, 5890])
+    cumulative_sales = np.cumsum(sales)
 
 # residual (error) function
 def residual(vars, t, sales):
