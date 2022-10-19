8d2afe293ee2db088c6ea571791415c311f6900c
reinvantveer <rein@vantveer.me>
2022-10-19 16:43:38 +0200

    add test assertion
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- tests/test_bass_model.py
+++ tests/test_bass_model.py
@@ -6,15 +6,15 @@ from models.bass_diffusion import BassDiffusionModel
 def test_model_predictions() -> None:
     model = BassDiffusionModel()
 
-    # expected_sales vector
-    expected_sales = np.array([840, 1470, 2110, 4000, 7590, 10950, 10530, 9470, 7790, 5890])
+    sales = np.array([840, 1470, 2110, 4000, 7590, 10950, 10530, 9470, 7790, 5890])
 
     # time interpolation
-    t = np.linspace(1.0, float(len(expected_sales)), num=10)
+    t = np.linspace(1.0, float(len(sales)), num=10)
 
     # time intervals
-    tp = np.linspace(0.1, float(len(expected_sales)), num=100)
+    tp = np.linspace(0.1, float(len(sales)), num=100)
 
-    model.fit(times=t, sales=expected_sales)
+    model.fit(times=t, sales=sales)
 
-    model.plot_sales_pdf(t, tp, expected_sales)
+    assert 70000 > model.bass_parameters.m > 60000
+    model.plot_sales_pdf(t, tp, sales)
