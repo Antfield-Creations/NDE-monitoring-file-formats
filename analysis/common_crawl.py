fix error calculation
Signed-off-by: reinvantveer <reinvantveer@gmail.com>

analysis/common_crawl.py
@@ -169,7 +169,9 @@ def analyse(stats: MimeDict, collection_metadata: List[Dict[str, str]], config: 
 
         # Project Bass model "sales"
         bass_fitted_values = bass_model.sales_at_time(bass_model.bass_parameters, bass_train_times)
-        bass_projected_values = bass_model.sales_at_time(bass_model.bass_parameters, bass_test_times)
+        bass_projected_ndarray = bass_model.sales_at_time(bass_model.bass_parameters, bass_test_times)
+        assert type(bass_projected_ndarray) == np.ndarray
+        bass_projected_values = bass_projected_ndarray.tolist()
 
         # Constrain the linear model to only the values from the highest value onwards
         max_idx = train_values.index(max(train_values))
@@ -183,7 +185,7 @@ def analyse(stats: MimeDict, collection_metadata: List[Dict[str, str]], config: 
         linear_projected_values = linear_model.predict(linear_test_times)
 
         # Calculate accuracy in average error
-        actual_test_values = list(usage_per_crawl[:test_crawls_idx])
+        actual_test_values = list(usage_per_crawl[test_crawls_idx:])
         assert type(actual_test_values) == list
         assert type(bass_projected_values) == list
 