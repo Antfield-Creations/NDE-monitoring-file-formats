1f7d0fb2ce0fdceff4e3c004493bd154403702ae
reinvantveer <rein@vantveer.me>
2022-10-31 15:28:24 +0100

    fix executable description
    Signed-off-by: reinvantveer <rein@vantveer.me>

--- analysis/nibg_aggregate.py
+++ analysis/nibg_aggregate.py
@@ -84,7 +84,7 @@ def main(config: Config) -> int:
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser('Performs the Nederlands Instituut voor Beeld en Geluid raw file archive metadata analysis')
+    parser = ArgumentParser('Performs the Nederlands Instituut voor Beeld en Geluid file archive metadata aggregation')
     parser.add_argument('-c', '--config', default='config.yaml')
 
     args = parser.parse_args()
