b204c07b6841e08352b8ddfafe5ceeca3b6b9a41
reinvantveer <reinvantveer@gmail.com>
2022-11-09 17:14:16 +0100

    add elaborate module description
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/scrape_dans.py
+++ analysis/scrape_dans.py
@@ -1,4 +1,24 @@
-import urllib.request
+"""
+Module for scraping file metadata from the DANS Archaeology Datastation.
+The scrape harvests only metadata from the files as originally deposited, not the files being offered as converted to
+preferred formats. This way, we can analyze the original file formats in use at the time of deposition.
+
+In order to extract this specific metadata, the `main` script filters data on the following:
+1. It iterates over all pages in the datasets index of the Archaeology Datastation (~120k results in ~12k pages)
+2. It extracts the digital object identifier (DOI) from the links to the datasets
+3. It rejects all datasets having a description "Files not yet migrated to Data Station"
+4. It accepts all datasets with - Version: either
+  - One version labeled "EASY Migration":
+    see https://archaeology.datastations.nl/dataset.xhtml?persistentId=doi:10.17026/dans-28e-mmdt
+  - Two or more versions labeled "EASY Migration":
+    see https://archaeology.datastations.nl/dataset.xhtml?persistentId=doi:10.17026/dans-x8z-d4he,
+  - Two or more versions, only the version with summary "This is the first published version.":
+    see: https://archaeology.datastations.nl/dataset.xhtml?persistentId=doi:10.17026/dans-zbe-b8h5
+6. For each dataset, for each file it extracts the deposit date
+7. It logs the result cursor and metadata in order to resume on error
+8. It aggregates the file metadata into a counter per file type, per month
+"""
+import json
 from argparse import ArgumentParser
 from math import ceil
 from typing import List
