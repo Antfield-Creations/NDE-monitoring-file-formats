4b130ca2d7d9254049cc63ac4b92ba632e6d1834
reinvantveer <reinvantveer@gmail.com>
2022-11-09 17:24:10 +0100

    type-hint, use refactored function name
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- tests/test_doi_extraction.py
+++ tests/test_doi_extraction.py
@@ -1,9 +1,9 @@
-from analysis.scrape_dans import parse_page
+from analysis.scrape_dans import extract_dois
 
 
-def test_doi_extraction():
+def test_doi_extraction() -> None:
     with open('tests/data/dans_page_data.html', 'rt') as f:
-        dois = parse_page(f.read())
+        dois = extract_dois(f.read())
 
     assert len(dois) == 10
     for doi in dois:
