37f2fbcf8a3ae83b1ea0aa9ca205a84ee9c92a42
reinvantveer <reinvantveer@gmail.com>
2022-11-10 14:51:13 +0100

    implement file metadata extraction, refactor to returning a tuple of (filenames, deposit_date)
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/scrape_dans.py
+++ analysis/scrape_dans.py
@@ -26,7 +26,7 @@ import json
 import logging
 from argparse import ArgumentParser
 from math import ceil
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Optional
 
 from bs4 import BeautifulSoup
 from tqdm import tqdm
@@ -54,14 +54,32 @@ def main(config: Config) -> int:
     page_size = int(results_count[2])
     num_pages = ceil(total / page_size)
 
+    num_skipped_datasets = 0
+    filetype_monthly_counts: Dict[str, Dict[str, int]] = {}
+
     for page_num in tqdm(range(dans_cfg['start_page'], num_pages)):
         dois = process_datasets_page(page_num, dans_cfg)
 
+        # Extract the file metadata for each dataset (by DOI)
         for doi in dois:
-            _ = extract_file_metadata(doi, dans_cfg)
-            continue
+            file_metadata = extract_file_metadata(doi, dans_cfg)
+            if file_metadata is None:
+                num_skipped_datasets += 1
+                continue
 
-        break
+            filenames, deposit_date = file_metadata
+            deposit_month = deposit_date[:6]  # YYYY-mm
+
+            # Update the monthly file extension counts by going over all the v1 files in the dataset
+            for file in filenames:
+                # Skip filenames without file extension
+                if '.' not in file:
+                    continue
+
+                extension = file.split('.')[-1]
+                filetype_monthly_counts.setdefault(extension, {})
+                filetype_monthly_counts[extension].setdefault(deposit_month, 0)
+                filetype_monthly_counts[extension][deposit_month] += 1
 
     return 0
 
@@ -105,21 +123,19 @@ def extract_dois(res_text: str) -> List[str]:
     return dois
 
 
-def extract_file_metadata(doi: str, dans_cfg: dict) -> List[Tuple[str, str]]:
+def extract_file_metadata(doi: str, dans_cfg: dict) -> Optional[Tuple[List[str], str]]:
     """
-    Extracts original file names and deposit dates for a dataset designated by `doi`
+    Extracts a list of original filenames and a deposit date for a dataset designated by `doi`
 
-    It returns an empty list for a dataset having a description "Files not yet migrated to Data Station"
-    It accepts a dataset with a valid version: either
-      - One version labeled "EASY Migration" and summary "This is the first published version."
-      - Two or more versions, only the version with summary "This is the first published version.":
-        see: https://archaeology.datastations.nl/dataset.xhtml?persistentId=doi:10.17026/dans-zbe-b8h5
-      - Two or more versions labeled "EASY Migration": TODO
+    It returns None for a dataset having
+        - a description "Files not yet migrated to Data Station"
+        - no two versions
+        - no version 1
 
     :param doi:         Digital object identifier for the dataset
     :param dans_cfg:    DANS Archaeology datastation extracted from a Config instance
 
-    :return:            A list of tuples, each containing a filename and a deposit date. May return an empty list.
+    :return:            A tuple of a list of filenames and a deposit date, or None if requirements ar not met.
     """
     root_url = dans_cfg['root_url']
     overview_subpath = dans_cfg['dataset_overview_api_subpath']
@@ -137,9 +153,33 @@ def extract_file_metadata(doi: str, dans_cfg: dict) -> List[Tuple[str, str]]:
     # Inspect the available dataset versions
     versions_subpath = dans_cfg['dataset_versions_api_subpath']
     url = root_url + versions_subpath.format(doi=doi)
-    _ = json.loads(get(url))
+    versions = json.loads(get(url))
 
-    return []
+    # Return an empty result if there are not two versions of the dataset: one of the original deposited data files,
+    # and one offering the data in preferred formats
+    if len(versions['data']) != 2:
+        logging.debug(f'Skipping: no two versions for {doi}')
+        return None
+
+    # Return an empty result if there is no single version 1 of the dataset
+    first_version_candidates = [version for version in versions['data'] if version['versionNumber'] == 1]
+    if len(first_version_candidates) != 1:
+        version_numbers = [version['versionNumber'] for version in versions['data']]
+        logging.error(f"Skipping {doi}, no version 1 in {version_numbers}")
+        return None
+
+    first_version = first_version_candidates[0]
+    citation_fields = first_version['metadataBlocks']['citation']['fields']
+    maybe_date_of_deposit = [field for field in citation_fields if field['typeName'] == 'dateOfDeposit']
+    # Return an empty result if there is not a singular deposit date
+    if len(maybe_date_of_deposit) != 1:
+        logging.error(f"Skipping {doi}: no date of deposit found")
+        return None
+
+    deposit_date = maybe_date_of_deposit[0]['value']
+    filenames = [file['label']for file in first_version['files']]
+
+    return filenames, deposit_date
 
 
 if __name__ == '__main__':
