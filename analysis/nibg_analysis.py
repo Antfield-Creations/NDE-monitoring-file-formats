e65e4d464a9fee8e2d773fe770a7d85320d8e417
reinvantveer <reinvantveer@gmail.com>
2022-11-11 10:28:15 +0100

    fit bass model
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/nibg_analysis.py
+++ analysis/nibg_analysis.py
@@ -34,7 +34,7 @@ def main(config: Config) -> int:
         aggregate_stats = json.loads(f.read())
 
     quarterly_counts = to_sorted_quarterly(aggregate_stats)
-    plot_counts(quarterly_counts, nibg_cfg['img_output_dir'])
+    plot_counts(quarterly_counts, nibg_cfg)
 
     end = datetime.datetime.now()
     logging.info(f'Script took {end - start}')
@@ -70,23 +70,43 @@ def to_sorted_quarterly(file_type_montly_counts: PeriodicFiletypeCount) -> Sorte
     return quarterly_counts
 
 
-def plot_counts(counts: SortedFileCount, output_dir: str) -> None:
+def plot_counts(counts: SortedFileCount, nibg_cfg: dict) -> None:
+    output_dir = nibg_cfg['img_output_dir']
+    num_tests = nibg_cfg['num_test_measurements']
+
     for file_type, quarter_counts in counts.items():
         quarters = [entry['period'] for entry in quarter_counts]
-        file_counts = [entry['count'] for entry in quarter_counts]
 
-        # Fit the Bass model
-        times = list(range(len(file_counts)))
+        # Set up training and test data
+        all_times = list(range(len(quarters)))
+        train_times = all_times[:-num_tests]
+        test_times = all_times[-num_tests:]
+
+        all_counts = [entry['count'] for entry in quarter_counts]
+        train_counts = all_counts[:-num_tests]
+        # test_counts = all_counts[-num_tests:]
+
+        # Fit the Bass model and data
         bass_model = BassDiffusionModel()
-        bass_model.fit(np.array(times), np.array(file_counts))
+        train_inputs = np.array(train_times)
+        test_inputs = np.array(test_times)
+        bass_model.fit(train_inputs, np.array(train_counts))
+        fitted_values = bass_model.sales_at_time(bass_model.bass_parameters, train_inputs)
+        projected_values = bass_model.sales_at_time(bass_model.bass_parameters, test_inputs)
 
         plt.plot(
-            times, file_counts
+            all_times, all_counts,
+            train_times, fitted_values,
+            test_times, projected_values
         )
         x_axis_labels = extract_year_ticks(quarters, separator='Q', index=0)
         plt.title(f"NIBG tellingen voor bestandstype {file_type}")
-        plt.xticks(times, x_axis_labels, rotation=45)
-        plt.legend(['Aantal bestanden'])
+        plt.xticks(all_times, x_axis_labels, rotation=45)
+        plt.legend([
+            'Aantal bestanden',
+            'Bass "fit"',
+            'Bass "test"'
+        ])
         plt.show()
         plt.savefig(os.path.join(output_dir, f'{file_type}.png'))
 
