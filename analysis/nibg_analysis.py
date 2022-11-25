0d5454b0d64d6446b5c19d544bdb4fa99343e33d
reinvantveer <reinvantveer@gmail.com>
2022-11-25 10:37:07 +0100

    refactor: move `plot_counts` function to shared module
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/nibg_analysis.py
+++ analysis/nibg_analysis.py
@@ -4,13 +4,8 @@ import logging
 import os
 from argparse import ArgumentParser
 
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn.linear_model import LinearRegression
-
 from analysis.config import load_config, Config
-from analysis.shared_parsers import extract_year_ticks, to_pruned_sorted_quarterly, SortedFileCount
-from models.bass_diffusion import BassDiffusionModel
+from analysis.shared_parsers import to_pruned_sorted_quarterly, plot_counts
 
 
 def main(config: Config) -> int:
@@ -28,67 +23,6 @@ def main(config: Config) -> int:
     logging.info(f'Script took {end - start}')
 
     return 0
-
-
-def plot_counts(counts: SortedFileCount, nibg_cfg: dict) -> None:
-    output_dir = nibg_cfg['img_output_dir']
-    num_tests = nibg_cfg['num_test_measurements']
-
-    for file_type, quarter_counts in counts.items():
-        quarters = [entry['period'] for entry in quarter_counts]
-
-        # Set up training and test data
-        all_times = list(range(len(quarters)))
-        train_times = all_times[:-num_tests]
-        test_times = all_times[-num_tests:]
-
-        all_counts = [entry['count'] for entry in quarter_counts]
-        train_counts = all_counts[:-num_tests]
-        # test_counts = all_counts[-num_tests:]
-
-        # Fit the Bass model and produce data
-        bass_model = BassDiffusionModel()
-        train_inputs = np.array(train_times)
-        test_inputs = np.array(test_times)
-        bass_model.fit(train_inputs, np.array(train_counts))
-        fitted_values = bass_model.sales_at_time(bass_model.bass_parameters, train_inputs)
-        projected_values = bass_model.sales_at_time(bass_model.bass_parameters, test_inputs)
-
-        plot_data = [
-            all_times, all_counts,
-            train_times, fitted_values,
-            test_times, projected_values
-        ]
-        legend_data = [
-            'Aantal bestanden',
-            'Bass "fit"',
-            'Bass "test"'
-        ]
-
-        if file_type in nibg_cfg['linear_plots']:
-            # Fit linear model for selected file formats
-            linear_model = LinearRegression()
-            max_idx = train_counts.index(max(train_counts))
-            linear_train_times = np.expand_dims(all_times[max_idx:-num_tests], 1)
-            linear_train_counts = all_counts[max_idx:-num_tests]
-            linear_test_times = np.expand_dims(all_times[-num_tests:], 1)
-
-            linear_model.fit(linear_train_times, linear_train_counts)
-            linear_fitted_values = linear_model.predict(linear_train_times)
-            linear_projected_values = linear_model.predict(linear_test_times)
-
-            plot_data.extend([linear_train_times, linear_fitted_values])
-            plot_data.extend([linear_test_times, linear_projected_values])
-            legend_data.append('Lineair "fit"')
-            legend_data.append('Lineair "test"')
-
-        plt.plot(*plot_data)
-        x_axis_labels = extract_year_ticks(quarters, separator='Q', index=0)
-        plt.title(f"NIBG tellingen voor bestandstype {file_type}")
-        plt.xticks(all_times, x_axis_labels, rotation=45)
-        plt.legend(legend_data)
-        plt.savefig(os.path.join(output_dir, f'{file_type}.png'))
-        plt.show()
 
 
 if __name__ == '__main__':
--- analysis/shared_parsers.py
+++ analysis/shared_parsers.py
@@ -1,8 +1,16 @@
 import datetime
 import logging
 import math
+import os
 import re
 from typing import List, Tuple, Dict, TypedDict
+from urllib.parse import quote_plus
+
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+from models import BassDiffusionModel
 
 
 class PeriodCount(TypedDict):
@@ -36,7 +44,7 @@ def extract_year_ticks(time_labels: List[str], separator: str = '-', index: int 
 
 def next_year_quarter(last_period: str) -> Tuple[int, int]:
     """
-    A small helper function to calculate the next quarter
+    A small helper function to calculate the next quarter.
 
     :param last_period: The quarter to calculate the next one for
 
@@ -108,3 +116,65 @@ def to_pruned_sorted_quarterly(file_type_montly_counts: PeriodicFiletypeCount) -
         quarterly_counts[file_type].pop(-1)
 
     return quarterly_counts
+
+
+def plot_counts(counts: SortedFileCount, cfg: dict) -> None:
+    output_dir = cfg['img_output_dir']
+    num_tests = cfg['num_test_measurements']
+
+    for file_type, quarter_counts in counts.items():
+        quarters = [entry['period'] for entry in quarter_counts]
+
+        # Set up training and test data
+        all_times = list(range(len(quarters)))
+        train_times = all_times[:-num_tests]
+        test_times = all_times[-num_tests:]
+
+        all_counts = [entry['count'] for entry in quarter_counts]
+        train_counts = all_counts[:-num_tests]
+        # test_counts = all_counts[-num_tests:]
+
+        # Fit the Bass model and produce data
+        bass_model = BassDiffusionModel()
+        train_inputs = np.array(train_times)
+        test_inputs = np.array(test_times)
+        bass_model.fit(train_inputs, np.array(train_counts))
+        fitted_values = bass_model.sales_at_time(bass_model.bass_parameters, train_inputs)
+        projected_values = bass_model.sales_at_time(bass_model.bass_parameters, test_inputs)
+
+        plot_data = [
+            all_times, all_counts,
+            train_times, fitted_values,
+            test_times, projected_values
+        ]
+        legend_data = [
+            'Aantal bestanden',
+            'Bass "fit"',
+            'Bass "test"'
+        ]
+
+        if file_type in cfg['linear_plots']:
+            # Fit linear model for selected file formats
+            linear_model = LinearRegression()
+            max_idx = train_counts.index(max(train_counts))
+            linear_train_times = np.expand_dims(all_times[max_idx:-num_tests], 1)
+            linear_train_counts = all_counts[max_idx:-num_tests]
+            linear_test_times = np.expand_dims(all_times[-num_tests:], 1)
+
+            linear_model.fit(linear_train_times, linear_train_counts)
+            linear_fitted_values = linear_model.predict(linear_train_times)
+            linear_projected_values = linear_model.predict(linear_test_times)
+
+            plot_data.extend([linear_train_times, linear_fitted_values])
+            plot_data.extend([linear_test_times, linear_projected_values])
+            legend_data.append('Lineair "fit"')
+            legend_data.append('Lineair "test"')
+
+        plt.plot(*plot_data)
+        x_axis_labels = extract_year_ticks(quarters, separator='Q', index=0)
+        plt.title(f"NIBG tellingen voor bestandstype {file_type}")
+        plt.xticks(all_times, x_axis_labels, rotation=45)
+        plt.legend(legend_data)
+        os.makedirs(output_dir, exist_ok=True)
+        plt.savefig(os.path.join(output_dir, f'{quote_plus(file_type)}.png'))
+        plt.show()
