84557a786d8f0b423af0f382fe4bb2b55e2ab8e5
reinvantveer <reinvantveer@gmail.com>
2022-11-28 13:00:52 +0100

    extract out filter function for file types to reduce complexity of `main()`
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- analysis/dans_analysis.py
+++ analysis/dans_analysis.py
@@ -6,7 +6,7 @@ from typing import List, Tuple
 import numpy as np
 
 from analysis.config import Config, load_config
-from analysis.shared_parsers import PeriodicFiletypeCount, plot_counts, to_sorted_yearly
+from analysis.shared_parsers import PeriodicFiletypeCount, plot_counts, to_sorted_yearly, SortedFileCount
 
 
 def main(config: Config) -> int:
@@ -20,7 +20,6 @@ def main(config: Config) -> int:
         filecount_sum += sum(counts.values())
 
     logging.info(f'Total file count: {filecount_sum}')
-
     logging.info(f'DANS analysis has {len(monthly_stats.keys())} file types (reverse-sorted by count:')
 
     filetype_counts: List[Tuple[str, int]] = []
@@ -33,9 +32,19 @@ def main(config: Config) -> int:
     yearly_stats = to_sorted_yearly(monthly_stats)
 
     # Keep only file types with more than the configured number of measurements and which are part of the selection
+    keep_filetypes = filter_stats(yearly_stats, dans_cfg)
+
+    logging.info(f'Keeping {len(keep_filetypes)} filetypes for analysis: {keep_filetypes}')
+    kept_counts = {filetype: counts for filetype, counts in yearly_stats.items() if filetype in keep_filetypes}
+    plot_counts(kept_counts, dans_cfg)
+
+    return 0
+
+
+def filter_stats(yearly_stats: SortedFileCount, dans_cfg: dict) -> List[str]:
     keep_filetypes: List[str] = []
-    for filetype, quarterly_counts in yearly_stats.items():
-        counts_reversed = list(reversed(quarterly_counts))
+    for filetype, yearly_counts in yearly_stats.items():
+        counts_reversed = list(reversed(yearly_counts))
 
         # prune 0-counts
         while counts_reversed[0]['count'] == 0:
@@ -45,11 +54,11 @@ def main(config: Config) -> int:
             continue
 
         # Take the last quarters to assess if the file type was in decline
-        maybe_declining_period = quarterly_counts[-dans_cfg['decline_periods']:]
-        quarterly_changes = np.diff([p['count'] for p in maybe_declining_period])
+        maybe_declining_period = yearly_counts[-dans_cfg['decline_periods']:]
+        yearly_changes = np.diff([p['count'] for p in maybe_declining_period])
 
         # Ignore the file types with stable or increasing number quarterly_changes
-        if quarterly_changes.mean() >= 0:
+        if yearly_changes.mean() >= 0:
             continue
 
         # We do the exercise above because the mime types included in the "mime_plots" list was decided based on the
@@ -57,14 +66,18 @@ def main(config: Config) -> int:
         if filetype not in dans_cfg['mime_plots']:
             continue
 
+        count_idx = 0
+        while count_idx < len(yearly_counts):
+            year = int(yearly_counts[count_idx]['period'][:5])
+            if year < dans_cfg['min_year']:
+                yearly_counts.pop(count_idx)
+            else:
+                count_idx += 1
+
         # Keep the rest
         keep_filetypes.append(filetype)
 
-    logging.info(f'Keeping {len(keep_filetypes)} filetypes for analysis: {keep_filetypes}')
-    kept_counts = {filetype: counts for filetype, counts in yearly_stats.items() if filetype in keep_filetypes}
-    plot_counts(kept_counts, dans_cfg)
-
-    return 0
+    return keep_filetypes
 
 
 if __name__ == '__main__':
