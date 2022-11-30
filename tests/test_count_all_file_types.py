59907a57764e691dd784dee4a9d0c4377e21a1af
reinvantveer <reinvantveer@gmail.com>
2022-11-30 11:23:35 +0100

    add different file type for testing, assert all periods are sorted
    Signed-off-by: reinvantveer <reinvantveer@gmail.com>

--- tests/test_count_all_file_types.py
+++ tests/test_count_all_file_types.py
@@ -5,8 +5,13 @@ def test_periodic_all_filetype_count_aggregation() -> None:
     test_data: SortedFileCount = {
         '.jpg': [
             {'period': '2018Q1', 'count': 1},
-            {'period': '2018Q2', 'count': 2},
             {'period': '2018Q3', 'count': 3},
+            {'period': '2018Q2', 'count': 2},
+        ],
+        '.tif': [
+            {'period': '2019Q1', 'count': 1},
+            {'period': '2017Q2', 'count': 3},
+            {'period': '2018Q2', 'count': 2},
         ]
     }
 
@@ -14,4 +19,8 @@ def test_periodic_all_filetype_count_aggregation() -> None:
     assert list(all_counts.keys()) == ['all']
 
     total_sum = sum(period_count['count'] for period_count in all_counts['all'])
-    assert total_sum == 6
+    assert total_sum == 12
+
+    # Assert that the list of periods is sorted by period
+    all_periods = [period_count['period'] for period_count in all_counts['all']]
+    assert all_periods == sorted(all_periods)
